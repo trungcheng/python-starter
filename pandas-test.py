1713d2ccd6eee8185245fbbee7117e0881381d8f
trung.dinh <trung.dinh@ntq-solution.com.vn>
2022-06-17 14:20:54 +0700

    pandas

---
+++ pandas-test.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# Series
+print('Series')
+a = [1, 7, 2]
+myvar = pd.Series(a)
+print(myvar)
+
+data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45]
+}
+
+# load data into a DataFrame object:
+print('dataframe')
+df = pd.DataFrame(data)
+print(df)
+
+# read csv
+print('csv')
+pd.options.display.max_rows = 9999
+df = pd.read_csv('data.csv')
+print(df)
+
+# read json
+print('json')
+df = pd.read_json('data.json')
+print(df.to_string())
